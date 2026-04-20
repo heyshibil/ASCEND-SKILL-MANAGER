fad71044ec67ba0249cda5cf6c537b102cc7c9ac
shibzz <monsterbegin@gmail.com>
2026-04-20 19:58:34 +0530

    feat(server-admin): implement bulk question seeding functionality with zod validation

--- server/src/modules/questions/questions.routes.ts
+++ server/src/modules/questions/questions.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { authenticate } from "../../middlewares/auth.middleware.js";
 import { isAdmin } from "../../middlewares/admin.middleware.js";
-import { createQuestion } from "./questions.controller.js";
+import { createQuestion, seedBulkQuestions } from "./questions.controller.js";
 
 const router = Router();
 
 router.post("/", authenticate, isAdmin, createQuestion);
+router.post("/bulk", authenticate, isAdmin, seedBulkQuestions)
 
 export default router;
